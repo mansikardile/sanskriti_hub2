1b5cccb22f8d94cacbf4397c98da54ce9a33b84b
Mansi Kardile <mansikardile@gmail.com>
2026-01-19 22:51:58 +0530

    add navigation bar

---
+++ sanskriti_hub/lib/main_navigation.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'learner_dashboard.dart';
+import 'artisan_marketplace.dart';
+import 'stories_screen.dart';
+
+class MainNavigation extends StatefulWidget {
+  const MainNavigation({super.key});
+
+  @override
+  State<MainNavigation> createState() => _MainNavigationState();
+}
+
+class _MainNavigationState extends State<MainNavigation> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const LearnerDashboard(),    // Image 1
+    const ArtisanMarketplace(),  // Image 2 & 6
+    const StoriesScreen(),       // Image 5 (Player) & 6 (Feed)
+    const Center(child: Text("Profile")),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: const Color(0xFF00796B),
+        unselectedItemColor: Colors.grey,
+        onTap: (index) => setState(() => _currentIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), activeIcon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.shopping_bag_outlined), activeIcon: Icon(Icons.shopping_bag), label: 'Shop'),
+          BottomNavigationBarItem(icon: Icon(Icons.auto_stories_outlined), activeIcon: Icon(Icons.auto_stories), label: 'Stories'),
+          BottomNavigationBarItem(icon: Icon(Icons.person_outline), activeIcon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
